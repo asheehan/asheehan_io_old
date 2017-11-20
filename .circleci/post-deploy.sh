cc0e113657e1fca8457cb78fecb4ad6c32ac86a4
Andrew Sheehan <sheehan.andrew@gmail.com>
2017-11-20 07:44:59 -0600

    Move prod app to port 8080

--- .circleci/post-deploy.sh
+++ .circleci/post-deploy.sh
@@ -21,7 +21,7 @@ ssh $APP_USER@$APP_HOST <<ENDSSH
   # sudo setcap 'cap_net_bind_service=+ep' /home/$APP_USER/$APP_NAME/erts-9.1.2/bin/beam.smp;
 
   echo "starting app";
-  sudo PORT=80 ./bin/$APP_NAME start >> deploy-log.txt;
+  sudo PORT=8080 ./bin/$APP_NAME start >> deploy-log.txt;
 
   echo "Finished";
 ENDSSH
